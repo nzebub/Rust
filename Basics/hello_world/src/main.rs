014bf694dc8cb77e5acc009ade427e0dfc6efdb2
Krishnaraj R <nzebub@gmail.com>
2017-12-07 20:15:36 +0530

    Hello World in Rust

---
+++ Basics/hello_world/src/main.rs
@@ -0,0 +1,12 @@
+//!
+//!
+//!     Hello World
+//!
+//!     in Rust Programming Language.
+//!
+//!     Dec 7 2017
+//!
+
+fn main() {
+    println!("Hello, world!");
+}
---
+++ Basics/hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+/home/krishna/Rust/Basics/hello_world/target/debug/hello_world: /home/krishna/Rust/Basics/hello_world/src/main.rs
